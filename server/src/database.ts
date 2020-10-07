bdab87be682861420946896da18948793d3cac1d
Jakub Zika <jakub.z@remoteorigin.co>
2020-10-07 19:07:45 +0200

    Add database provider

---
+++ server/src/database.ts
@@ -0,0 +1,38 @@
+import { Connection, createConnection } from 'typeorm'
+import { DatabaseConfig, Configuration } from './configuration'
+import { entities } from './entity'
+
+export class DatabaseProvider {
+    static connection: Connection
+
+    /**
+     * Initialization of database connection
+     */
+    public static async initialize() {
+        const config: DatabaseConfig = Configuration.getDatabaseConfig()
+        DatabaseProvider.connection = await createConnection({
+            type: 'postgres',
+            host: config.host,
+            port: config.port,
+            username: config.username,
+            password: config.password,
+            database: config.database,
+            entities: entities,
+            synchronize: true,
+        })
+    }
+    /**
+     * @returns Promise with Connection database handler
+     */
+    public static async getConnection(): Promise<Connection> {
+        if (DatabaseProvider.connection) {
+            return DatabaseProvider.connection
+        }
+        try {
+            await DatabaseProvider.initialize()
+            return DatabaseProvider.connection
+        } catch (error) {
+            throw new Error('could not initialize database connection: ' + error)
+        }
+    }
+}
