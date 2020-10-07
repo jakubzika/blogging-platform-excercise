60d5f2e110fedeb361758d86f5bf5095aacc202b
Jakub Zika <jakub.z@remoteorigin.co>
2020-10-07 14:32:44 +0200

    Add support for yaml configuration file

---
+++ server/src/config.ts
@@ -0,0 +1,9 @@
+export interface Configuration {
+    database: {
+        host: string
+        username: string
+        password: string
+        port: number
+        database: string
+    }
+}
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,3 +1,26 @@
-import "reflect-metadata";
+import 'reflect-metadata'
+import YAML from 'yaml'
+import fs from 'fs'
+import { createConnection } from 'typeorm'
 
-console.log('hello world')
+import { Configuration } from './config'
+
+const configuration: Configuration = YAML.parse(fs.readFileSync('../config.yml', 'utf8'))
+const dbConfig = configuration.database
+
+createConnection({
+    type: 'postgres',
+    host: dbConfig.host,
+    port: dbConfig.port,
+    username: dbConfig.username,
+    password: dbConfig.password,
+    database: dbConfig.database,
+    entities: [__dirname + '/entity/*.ts'],
+    synchronize: true,
+})
+    .then((connection) => {
+        console.log('started')
+    })
+    .catch((err) => {
+        console.log(err)
+    })
