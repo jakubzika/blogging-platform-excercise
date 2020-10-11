9f334d65151eda98e2e5765c9a42f31ce2d710e7
Jakub Zika <jakub.z@remoteorigin.co>
2020-10-11 11:32:34 +0200

    Add controller for user with login and user creation

---
+++ server/src/controllers/user.ts
@@ -0,0 +1,82 @@
+import { Next, Request, Response } from 'restify'
+import errors from 'restify-errors'
+import jwt from 'jsonwebtoken'
+
+import { User } from '../entity/user'
+import { RouteHandler } from './route'
+import { Router } from '../router'
+import { Configuration } from '../configuration'
+import {
+    UserManager,
+    LoginCredentials,
+    UserAuthenticationError,
+    UserCreationObject,
+    UserCreationError,
+} from '../services/userManager'
+
+export interface JwtData {
+    uid: number
+    email: string
+}
+
+export class UserController implements RouteHandler {
+    jwtSecret: string
+    constructor() {
+        this.jwtSecret = Configuration.getJwtSecret()
+        console.log('jwtSecret', this.jwtSecret)
+    }
+
+    registerHandler(router: Router) {
+        router.setBase('/user')
+
+        router.post('/login', this.auth.bind(this))
+        router.post('/', this.create.bind(this))
+    }
+
+    async auth(req: Request, res: Response, next: Next) {
+        const login: LoginCredentials = req.body
+
+        const result = await UserManager.authenticate(login)
+        if (result instanceof User) {
+            const data: JwtData = {
+                uid: result.id,
+                email: result.email,
+            }
+
+            const token = jwt.sign(data, this.jwtSecret, {
+                expiresIn: '30m',
+            })
+
+            res.send({ token })
+            next()
+        } else {
+            if (
+                result == UserAuthenticationError.NO_ACCOUNT_WITH_EMAIL ||
+                result == UserAuthenticationError.PASSWORDS_DONT_MATCH
+            )
+                next(new errors.InvalidCredentialsError('Invalid email or password'))
+            else {
+                next(new errors.InternalError(''))
+            }
+        }
+    }
+
+    async create(req: Request, res: Response, next: Next) {
+        const creation: UserCreationObject = req.body
+        console.log('creation', creation)
+
+        try {
+            const result = await UserManager.createuser(creation)
+            if (result instanceof User) {
+                res.send({ uid: result.id })
+                next()
+            } else {
+                if (result == UserCreationError.USER_ALREADY_EXISTS) {
+                    next(new errors.InvalidCredentialsError('Email already registered'))
+                }
+            }
+        } catch {
+            next(new errors.InternalServerError(''))
+        }
+    }
+}
