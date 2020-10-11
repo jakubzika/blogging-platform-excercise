51678042ef2238230807982ccf7f5aff81a40f00
Jakub Zika <jakub.z@remoteorigin.co>
2020-10-11 17:37:42 +0200

    First webpack config

---
+++ client/webpack.config.js
@@ -0,0 +1,68 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'none',
+    entry: {
+        app: path.join(__dirname, 'src', 'index.tsx')
+    },
+    target: 'web',
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: '/node_modules/'
+            },
+            {
+                test: /\.css$/i,
+                use: [
+                  "style-loader",
+                  "@teamsupercell/typings-for-css-modules-loader",
+                  {
+                    loader: "css-loader",
+                    options: { modules: true }
+                  },
+                  {
+                    loader: 'sass-loader',
+                    options: {
+                      // Prefer `dart-sass`
+                      implementation: require('sass'),
+                    },
+                  },
+                ]
+            },
+            {
+              test: /\.(png|svg|jpg|gif)$/,
+              use: [
+                'file-loader',
+              ],
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    'file-loader',
+                ],
+            }, 
+        ],
+    },
+    devServer: {
+        contentBase: path.join('../dist/dist'),
+        port: 9000,
+        historyApiFallback: true,
+        // publicPath: '/assets/'
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve('../dist/client'),
+        publicPath: '/',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'src', 'index.html')
+        })
+    ]
+}
