9e9278c4ba76332e2107602afb4917794fa49b66
Jakub Zika <jakub.z@remoteorigin.co>
2020-10-11 11:35:29 +0200

    Add options to router, add option to require authorization for the route

--- server/src/router.ts
+++ server/src/router.ts
@@ -1,38 +1,73 @@
 import { Server, RequestHandler } from 'restify'
+import restifyJwt from 'restify-jwt-community'
+import { Configuration } from './configuration'
+
+export interface RouterOptions {
+    authentication?: Boolean
+}
 
 export class Router {
     server: Server
     base: string
+    jwtSecret: string
 
     constructor(server: Server) {
         this.server = server
+        this.jwtSecret = Configuration.getJwtSecret()
     }
 
     setBase(base: string) {
         this.base = base
     }
 
-    get(path: string, ...handlers: RequestHandler[]) {
-        this.addRoute('get', path, handlers)
+    get(path: string, handlers: RequestHandler | RequestHandler[], options: RouterOptions = {}) {
+        this.addRoute('get', path, handlers, options)
     }
-    post(path: string, ...handlers: RequestHandler[]) {
-        this.addRoute('post', path, handlers)
+    post(path: string, handlers: RequestHandler | RequestHandler[], options: RouterOptions = {}) {
+        this.addRoute('post', path, handlers, options)
     }
-    put(path: string, ...handlers: RequestHandler[]) {
-        this.addRoute('put', path, handlers)
+    put(path: string, handlers: RequestHandler | RequestHandler[], options: RouterOptions = {}) {
+        this.addRoute('put', path, handlers, options)
     }
-    patch(path: string, ...handlers: RequestHandler[]) {
-        this.addRoute('patch', path, handlers)
+    patch(path: string, handlers: RequestHandler | RequestHandler[], options: RouterOptions = {}) {
+        this.addRoute('patch', path, handlers, options)
     }
-    del(path: string, ...handlers: RequestHandler[]) {
-        this.addRoute('del', path, handlers)
+    del(path: string, handlers: RequestHandler | RequestHandler[], options: RouterOptions = {}) {
+        this.addRoute('del', path, handlers, options)
     }
+
+    // post(path: string, ...handlers: RequestHandler[]) {
+    //     this.addRoute('post', path, handlers)
+    // }
+    // put(path: string, ...handlers: RequestHandler[]) {
+    //     this.addRoute('put', path, handlers)
+    // }
+    // patch(path: string, ...handlers: RequestHandler[]) {
+    //     this.addRoute('patch', path, handlers)
+    // }
+    // del(path: string, ...handlers: RequestHandler[]) {
+    //     this.addRoute('del', path, handlers)
+    // }
+
+    authPlugin
 
     private addRoute(
         method: 'get' | 'post' | 'put' | 'patch' | 'del',
         path: string,
-        handlers: RequestHandler[]
+        handler: RequestHandler | RequestHandler[],
+        options: RouterOptions
     ) {
-        this.server[method](`${this.base}${path}`, ...handlers)
+        let handlers: RequestHandler[]
+        if (Array.isArray(handler)) {
+            handlers = handler
+        } else {
+            handlers = [handler]
+        }
+
+        if (options.authentication) {
+            handlers.unshift(restifyJwt({ secret: this.jwtSecret }))
+        }
+
+        this.server[method](`${this.base}${path}`, handlers)
     }
 }
